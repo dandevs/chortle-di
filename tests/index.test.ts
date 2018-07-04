2b8d25d98e3bf624e4e4e2ee240343afa45761d5
dandevs <danilodevs@gmail.com>
2018-07-04 19:45:46 -0400

    Initial version of new version

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,56 +1,38 @@
-import { inject, override, injectable } from "../src";
-let i = 0;
+import { inject, injectable } from "../src";
 
-class Bar {
-    value = i++;
-}
+test("Can inject", () => {
+    class B { value = Math.random(); }
 
-class Baz {
-    value = "bazinga...";
-}
-
-class Logger {}
-
-@injectable class Foo {
-    @inject(Bar) bar: Bar;
-
-    constructor(public value?) { }
-}
-
-it("Can inject dependencies", () => {
-    const a = new Foo("hello"),
-          b = new Foo("world");
-
-    expect(a.value).toBe("hello");
-    expect(b.value).toBe("world");
-
-    // Make sure it isn't instantiating new deps
-
-    expect(a.bar === b.bar).toBe(false);
-    expect(a.bar.value).toBe(a.bar.value);
-    expect(a.bar.value === b.bar.value).toBe(false);
-});
-
-it.only("#override", () => {
-    override(Foo, "bar", Baz);
-    // const pre = new Foo("pre");
-    // expect(typeof pre.bar.value).toBe("number");
-
-    // override(Foo, "bar", Baz);
-    // const post = new Foo("post");
-
-    // expect(typeof post.bar.value).toBe("string");
-});
-
-it("Multi dependency injection", () => {
-    class B {}
-    class C {}
+    class C {
+        constructor(public value?: any) {}
+    }
 
     @injectable class A {
         @inject(B) b: B;
-        @inject(C) c: C;
+        @inject(C, "foobar") c: C;
+
+        constructor(public value?: string) {}
     }
 
-    // @ts-ignore
-    new A().$di; //?
+    const a0 = new A("hello");
+    const a1 = new A("world");
+
+    expect(a0.b === a1.b).toBe(false);
+    expect(a0.value).toBe("hello");
+    expect(a1.value).toBe("world");
+
+    expect(a0.c.value).toBe("foobar");
+});
+
+test("#inject.singleton()", () => {
+    class A {}
+
+    @injectable class B {
+        @inject.singleton(A) a: A;
+    }
+
+    const i0 = new B();
+    const i1 = new B();
+
+    expect(i0.a === i1.a).toBe(true);
 });
